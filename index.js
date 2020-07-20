3837675e66d366447175358be4ac915cd075251a
JanBoeker <naj_b@gmx.de>
2020-07-20 22:34:23 +0200

    initial setup jQuery-project

---
+++ index.js
@@ -0,0 +1,8 @@
+
+// The ready-functions waits until the jQuery-Library is loaded completly
+// jQuery(document).ready(function() {
+//   $("h1").css("color", "red");
+// });
+
+// We do not need to use the ready-function, if we place the includes of the scripts at the end of the body.
+$("h1").css("color", "red");
